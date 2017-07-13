1bfe10708d05bace6a4ba56545e86f96527eb98e
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2017-07-13 11:48:22 +0200

    optimize code; changed export in version.js to ES6 export

--- index.js
+++ index.js
@@ -14,12 +14,10 @@
  * Date: March 2015
  */
 
- var fs = require('fs');
- var exec = require('child_process').exec;
- var child = exec('git reflog --decorate -1', function (error, stdout, stderr) 
- {
- 	if (error)
- 	{
+var fs = require('fs');
+var exec = require('child_process').exec;
+var child = exec('git reflog --decorate -1', function (error, stdout, stderr) {
+	if (error) {
 		// Shit
 		console.log('[FAILED]: Failed to run Git command');
 		process.exit(1);
@@ -28,43 +26,36 @@
 	// Example output: a32d6d8 (HEAD, tag: TAG-V.02, tag: TAG-V.01, master) HEAD@{0}: commit (initial): Asd
 	// Run regular expression to extract sha and tag
 	var sha = stdout.match(/[a-z0-9]+\s\(HEAD/g);
-	if (sha && sha.length > 0)
-	{
+	if (sha && sha.length > 0) {
 		sha = sha[0].slice(0, -6);
 	}
 
 	var tag = stdout.match(/tag\:\s[a-zA-Z0-9\-\.]+\,/g);
-	if (tag && tag.length > 0)
-	{
+	if (tag && tag.length > 0) {
 		tag = tag[0].slice(5, -1);
 	}
 
 	// Compose version file info
-	var versionInfo = 'module.exports = {';
-	
-	if (tag)
-	{
-		versionInfo += '\n\ttag: \'' + tag + '\',';
-	}
-	else
-	{
-		versionInfo += '\n\ttag: null,';
-	}
-
-	versionInfo += '\n\thash: \'' + sha + '\',';
-	versionInfo += '\n\ttimestamp: ' + Math.floor(new Date().getTime()/1000);
-	versionInfo += '\n};\n';
+	const versionInfo = {
+		tag: tag ? tag : null,
+		hash: sha,
+		timestamp: Math.floor(new Date().getTime() / 1000)
+	};
 
 	// Create version.js file
-	fs.writeFile('version.js', versionInfo, function(err) 
-	{
-		if(err) 
-		{
+	fs.writeFile('version.js', buildModuleString(versionInfo), function (err) {
+		if (err) {
 			console.log('[FAILED]: can\'t create version.js file. Permission issue?');
 		}
-		else
-		{
+		else {
 			console.log('[OK]');
 		}
 	});
 });
+
+function buildModuleString(versionInfo) {
+	const versionInfoString = JSON.stringify(versionInfo);
+	const moduleString = 'export const version = ' + versionInfoString.substr(1, versionInfoString.length - 2) + ';';
+
+	return moduleString;
+}
