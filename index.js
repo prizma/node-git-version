62ef2e02dd9321fbfcdae72565f2cf77150172a3
BilliAlpha <billi.pamege.300@gmail.com>
2018-05-26 12:39:57 +0200

    Parse more infos

--- index.js
+++ index.js
@@ -12,59 +12,90 @@
  *
  * Created: Maxim Stepanov
  * Date: March 2015
+ * Modified: BilliAlpha
+ * Date: May 2018
  */
 
- var fs = require('fs');
- var exec = require('child_process').exec;
- var child = exec('git reflog --decorate -1', function (error, stdout, stderr) 
- {
- 	if (error)
- 	{
-		// Shit
-		console.log('[FAILED]: Failed to run Git command');
-		process.exit(1);
-	}
+var fs = require('fs');
+var exec = require('child_process').exec;
 
-	// Example output: a32d6d8 (HEAD, tag: TAG-V.02, tag: TAG-V.01, master) HEAD@{0}: commit (initial): Asd
-	// Run regular expression to extract sha and tag
-	var sha = stdout.match(/[a-z0-9]+\s\(HEAD/g);
-	if (sha && sha.length > 0)
-	{
-		sha = sha[0].slice(0, -6);
-	}
-
-	var tag = stdout.match(/tag\:\s[a-zA-Z0-9\-\.]+\,/g);
-	if (tag && tag.length > 0)
-	{
-		tag = tag[0].slice(5, -1);
-	}
-
-	// Compose version file info
-	var versionInfo = 'module.exports = {';
+module.exports = function (opts) {
+	var versionInfo = {};
 	
-	if (tag)
-	{
-		versionInfo += '\n\ttag: \'' + tag + '\',';
-	}
-	else
-	{
-		versionInfo += '\n\ttag: null,';
-	}
-
-	versionInfo += '\n\thash: \'' + sha + '\',';
-	versionInfo += '\n\ttimestamp: ' + Math.floor(new Date().getTime()/1000);
-	versionInfo += '\n};\n';
-
-	// Create version.js file
-	fs.writeFile('version.js', versionInfo, function(err) 
-	{
-		if(err) 
-		{
-			console.log('[FAILED]: can\'t create version.js file. Permission issue?');
+	var child = exec('git log --decorate -1 --date=unix', function (error, stdout, stderr) {
+		if (error) {
+			// Shit
+			console.log('[FAILED]: Failed to run Git command');
+			process.exit(error.code);
 		}
-		else
-		{
-			console.log('[OK]');
+
+		// Example output:
+		//
+		// commit 6f78514ee4c055453ac8effba2680b5fd5304f04 (HEAD -> master, tag: v1.1.0, origin/master)
+		// Merge: db7fb4a 0b5a3e4
+		// Author: BilliAlpha <???>
+		// Date:   Fri May 25 21:50:04 2018 +0200
+		//
+		//     Merge branch 'master'
+		//
+		
+		var data = stdout.split('\n');
+		
+		// Run regular expression to extract firstLine parts
+		var firstLine = data[0].match(/commit ([a-z0-9]{40})(?: \((.+)\))?/g);
+		
+		// Get commit id
+		if (firstLine && firstLine.length > 0) {
+			versionInfo.commit = firstLine[0];
+			versionInfo.shortCommit = versionInfo.commit.slice(0,7);
 		}
+
+		// Parse decorate infos
+		versionInfo.tags = [];
+		if (firstLine && firstLine.length > 1) {
+			var decorate = firstLine[1].split(',');
+			for (var d=0; d<decorate.length; d++) {
+				var e = decorate[d].trim();
+				if (e.startsWith('tag: ')) { // Get tags
+					versionInfo.tags.push(e.substring(5));
+				} else if (e.startsWith('HEAD')) {
+					var arrow = e.indexOf('->');
+					if (arrow!==-1) {
+						versionInfo.branch = e.substring(arrow+1);
+					}
+				} else if (d===decorate.length-1) {
+					versionInfo.remoteBranch = e;
+				}
+			}
+		}
+
+		for (var l=1; l<data.length; l++) {
+			var line = data[l];
+			if (line.startsWith('Merge:')) {
+				versionInfo.merge = true;
+				var mergeCommits = line.substring(-15).split(' ');
+				versionInfo.mergeCommits = mergeCommits;
+			} else if (line.startsWith('Author:')) {
+				versionInfo.author = line.split(':')[1].trim();
+				var authorParts = versionInfo.author.split('<');
+				versionInfo.authorName = authorParts[0].trim()
+				versionInfo.authorMail = authorParts[1].substring(0,-1);
+			} else if (line.startsWith('Date:')) {
+				versionInfo.date = line.split(':')[1].trim();
+			} else if (line.startsWith('    ')) {
+				versionInfo.message = line;	
+			}
+		}
+
+		// Compose version file info
+		var fileContents = 'module.exports = '+JSON.stringify(versionInfo, null, 2)+';\n';
+		// Create git-version.js file
+		fs.writeFile('git-version.js', fileContents, function(err) {
+			if(err) {
+				console.log('[FAILED]: can\'t create git-version.js file. Permission issue?');
+			} else {
+				console.log('[OK]');
+			}
+		});
 	});
-});
+}
